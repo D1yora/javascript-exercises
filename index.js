71735047e285bf1074bdb58a48783b172dcf3b86
D1yora <dioora2o3@gmail.com>
2023-04-10 23:39:46 +0500

    Type Conversions | Operators | Comparison | Logical Operators

---
+++ index.js
@@ -0,0 +1,20 @@
+// 2 - dars | Type Conversions | Operators | Comparison | Logical Operators
+// homework
+
+//
+// let a = 1, b = 2;
+// let c = ++a;
+
+// console da 2 chiqadi. Sababi raqamlarni oldidan ++ qo'yadigan bo'lsak, birinchi qiymatini oshiradi, so'ngra console'da javobini chiqarib beradi.
+
+// let d = a++;
+// console'da javob 1 chiqadi, sabab c=birinchi console'da a ni chiqarib, keyin qiymatini birga oshiradi.
+
+// 3
+let a =2;
+let x =1 + (a *= 2)
+console.log(x); // 5
+// bu yerda birjnchi qavs ichidagi amal bajarildi, 2 * 2 = 4, i keyin 1 ga 4 qo'shildi, javob : 5
+
+// 4
+console.log("" + 1 + 0);
