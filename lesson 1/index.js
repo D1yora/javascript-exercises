cdba7e75c1a6c3a21347d637ace3e97b62310aad
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-04-15 00:58:27 +0500

    variable, data types

---
+++ lesson 1/index.js
@@ -0,0 +1,46 @@
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+
+// let
+// var
+// const
+
+// let a = 5; //local
+// var b = 6; // global
+// const c = 7; // o'zgarmas
+
+// NUmber, BigInt, Boolean,
+// string "", '', ``
+// null
+// undefined
+// let e = Symbol(2);
+// let d;
+// console.log(d);
+// let obj = {};
+// obj, array, function
+
+// primitive & non-primitive
+
+// <!-- // null bilan undefined
+  //  null'da value yo'q
+  //  undefined'da bor
+
+
+  // == boolean
+  // === -> true false
+  // console.log(null == undefined);
+  // console.log(null === undefined);
+  // console.log(isNaN(5 + null));  false
+  // console.log(isNaN(5 + undefined)); true -->
+
+console.log(+"5");
+console.log(Number("5"));
+let abc = "5c";
+console.log(isNaN(6 + undefined)); // not a num
+console.log(isNaN(6 + null)); // num + null = num
