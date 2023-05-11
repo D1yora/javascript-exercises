13f9a8264b305785db4b800c3635ded3cd67c124
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-05-11 11:39:20 +0500

    advanced array

--- lesson 11/index.js
+++ lesson 11/index.js
@@ -8,4 +8,24 @@ let users = [
   {id: 5, year: 2012, engine: 2, name: "Malibu"},
   {id: 6, year: 2000, engine: 1.2, name: "Damas"},
   {id: 7, year: 2018, engine: 2.4, name: "Tracker"},
-]
+];
+
+let res1 = users.filter((v) => v.year < 2000);
+let res2 = users.filter((v) => v.year > 2010);
+let res3 = users.sort((a,b) => a.engine - b.engine);
+let res4 = users.sort((a,b) => a.year - b.year);
+let res5 = users.sort((a,b) => a.name.localeCompare(b.name))
+let res6 = users.filter((v) => v.year < 2000);
+let res7 = users.filter((v) => v.year > 2000 && v.year < 2010);
+let res8 = users.filter((v) => v.year > 2010 && v.year < 2022);
+let statuslar = {
+  status: "eski ma'lumot",
+  status2: "o'rta ma'lumot",
+  status3: "yangi ma'lumot",
+}
+res6.push(statuslar.status);
+res7.push(statuslar.status2);
+res8.push(statuslar.status3);
+console.log(res6);
+console.log(res7);
+console.log(res8);
